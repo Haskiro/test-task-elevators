2695e01045881611814247d65713365426658d05
Павел <58857483+Haskiro@users.noreply.github.com>
2022-12-12 18:43:19 +0300

    expandable grid created

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <ul class="list" ref="mainBlock">
+      <li
+        class="item"
+        :style="{ height: calcFloorHeight() + 'px' }"
+        v-for="n in floorCount"
+        :key="n"
+      >
+        <div
+          class="col"
+          :style="{
+            width:
+              calcElevatorWidth() > 100
+                ? 150 + 'px'
+                : calcElevatorWidth() + 'px',
+          }"
+          v-for="n in elevatorCount"
+          :key="n"
+        ></div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "../styles/reset.scss";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      floorCount: 5,
+      elevatorCount: 1,
+      windowHeight: null,
+      windowWidth: null,
+    };
+  },
+  mounted() {
+    this.matchHeightAndWidth();
+  },
+  methods: {
+    matchHeightAndWidth: function () {
+      this.windowHeight = this.$refs.mainBlock.clientHeight;
+      this.windowWidth = this.$refs.mainBlock.clientWidth;
+    },
+    calcFloorHeight: function () {
+      return Math.floor(this.windowHeight / this.floorCount);
+    },
+    calcElevatorWidth: function () {
+      return Math.floor(this.windowWidth / this.elevatorCount);
+    },
+  },
+  computed: {},
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.list {
+  height: 100vh;
+}
+
+.item {
+  border: 1px solid lightgrey;
+  display: flex;
+}
+
+.col {
+  border-right: 2px solid black;
+  border-left: 2px solid black;
+  height: 100%;
+  box-sizing: content-box;
 }
 </style>
